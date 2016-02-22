1a94a63725095be4c60020ac9f4f273bf1894d13
Michael Schaefermeyer <michael.schaefermeyer@gmail.com>
2016-02-22 14:10:31 -0800

    Add bugsnag to dependencies

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Toolex.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :bugsnag]]
   end
 
   # Dependencies can be Hex packages:
